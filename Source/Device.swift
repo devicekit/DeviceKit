9f573d392bdd6773735a99f9b355b2ad8620f626
phiren <hiren.gujarati9@gmail.com>
2016-03-25 15:14:51 +0530

    Fixed Xcode 7.3 issue
    Xcode 7.3 issue

--- Source/Device.swift
+++ Source/Device.swift
@@ -356,7 +356,6 @@ public enum Device {
             case .Phone:        self = .Phone
             case .TV:           self = .TV
             case .CarPlay:      self = .CarPlay
-            case .Unspecified:  self = .Unspecified
             default:            self = .Unspecified
             }
         }
