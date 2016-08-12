2b459bdc241d605a349b204cf31ab7bf976446c8
Dennis Weissmann <dennis@denniss-imac.local>
2016-08-12 22:00:47 +0200

    unify targets into one framework target

---
+++ DeviceKit.h
@@ -0,0 +1,19 @@
+//
+//  DeviceKit.h
+//  DeviceKit
+//
+//  Created by Dennis Weissmann on 8/12/16.
+//
+//
+
+@import Foundation;
+
+//! Project version number for DeviceKit.
+FOUNDATION_EXPORT double DeviceKitVersionNumber;
+
+//! Project version string for DeviceKit.
+FOUNDATION_EXPORT const unsigned char DeviceKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeviceKit/PublicHeader.h>
+
+
