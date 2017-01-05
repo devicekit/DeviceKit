97d29d32421d3fe7d21d36ef1edfa949fcbbb169
Kaden Wilkinson <wilkinson.kaden@gmail.com>
2017-01-04 17:35:35 -0700

    Fix crash for looking up sim id on real device

--- Source/Device.swift
+++ Source/Device.swift
@@ -239,15 +239,13 @@ public enum Device {
       case "iPad5,1", "iPad5,2":                      return iPadMini4
       case "iPad6,3", "iPad6,4":                      return iPadPro9Inch
       case "iPad6,7", "iPad6,8":                      return iPadPro12Inch
-      // swiftlint:disable:next force_unwrapping
-      case "i386", "x86_64":                          return simulator(mapToDevice(identifier: String(validatingUTF8: getenv("SIMULATOR_MODEL_IDENTIFIER"))!))
+      case "i386", "x86_64":                          return simulator(mapToDevice(identifier: String(validatingUTF8: getenv("SIMULATOR_MODEL_IDENTIFIER")) ?? identifier))
       default:                                        return unknown(identifier)
       }
     #elseif os(tvOS)
       switch identifier {
       case "AppleTV5,3":                              return appleTV4
-      // swiftlint:disable:next force_unwrapping
-      case "i386", "x86_64":                          return simulator(mapToDevice(identifier: String(validatingUTF8: getenv("SIMULATOR_MODEL_IDENTIFIER"))!))
+      case "i386", "x86_64":                          return simulator(mapToDevice(identifier: String(validatingUTF8: getenv("SIMULATOR_MODEL_IDENTIFIER")) ?? identifier))
       default:                                        return unknown(identifier)
       }
     #endif
