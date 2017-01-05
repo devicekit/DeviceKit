fd3e44a8c7024c113be273b91c460a12225ea60c
Kaden Wilkinson <wilkinson.kaden@gmail.com>
2017-01-04 17:51:43 -0700

    Use better API to get env var that won't crash

--- Source/Device.swift
+++ Source/Device.swift
@@ -240,13 +240,13 @@ public enum Device {
       case "iPad5,1", "iPad5,2":                      return iPadMini4
       case "iPad6,3", "iPad6,4":                      return iPadPro9Inch
       case "iPad6,7", "iPad6,8":                      return iPadPro12Inch
-      case "i386", "x86_64":                          return Simulator(mapIdentifierToDevice(String(UTF8String: getenv("SIMULATOR_MODEL_IDENTIFIER")) ?? identifier))
+      case "i386", "x86_64":                          return Simulator(mapIdentifierToDevice(NSProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"] ?? "Unknown"))
       default:                                        return UnknownDevice(identifier)
       }
     #elseif os(tvOS)
       switch identifier {
       case "AppleTV5,3":                              return AppleTV4
-      case "i386", "x86_64":                          return Simulator(mapIdentifierToDevice(String(UTF8String: getenv("SIMULATOR_MODEL_IDENTIFIER")) ?? identifier))
+      case "i386", "x86_64":                          return Simulator(mapIdentifierToDevice(NSProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"] ?? "Unknown"))
       default:                                        return UnknownDevice(identifier)
       }
     #endif
