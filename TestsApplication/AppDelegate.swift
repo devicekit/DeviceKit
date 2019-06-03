baa67f0eeea4463057d5301b0e9ba3a1b14801fb
Zandor Smith <info@zsinfo.nl>
2019-06-03 18:23:21 +0200

    Add TestsApplication for running Unit Tests on real devices.

---
+++ TestsApplication/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  TestsApplication
+//
+//  Created by Zandor Smith on 03/06/2019.
+//  Copyright © 2019 Dennis Weissmann. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+
+}
---
+++ TestsApplication/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  TestsApplication
+//
+//  Created by Zandor Smith on 03/06/2019.
+//  Copyright © 2019 Dennis Weissmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+}
