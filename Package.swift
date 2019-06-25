4d68a49de872661aac1fea26b0b8940b25ce162b
Roberto Casula <rcasula3@gmail.com>
2019-06-25 17:11:05 +0200

    Changed default Sources directory

--- Package.swift
+++ Package.swift
@@ -25,7 +25,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "DeviceKit",
-            dependencies: []),
+            dependencies: [],
+            path: "Source"),
         .testTarget(
             name: "DeviceKitTests",
             dependencies: ["DeviceKit"],
