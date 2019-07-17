847c2d8321586aedb0cb449f91f7aa23031f6bf1
Roberto Casula <rcasula3@gmail.com>
2019-07-17 07:07:54 +0200

    Added supported platforms

--- Package.swift
+++ Package.swift
@@ -14,6 +14,11 @@ import PackageDescription
 
 let package = Package(
     name: "DeviceKit",
+    platforms: [
+        .iOS(.v8),
+        .tvOS(.v9),
+        .watchOS(.v2)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
