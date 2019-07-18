89452446badb4391899e989b8ae99c84488457f5
robcas3 <robcas3@users.noreply.github.com>
2019-07-18 22:00:26 +0200

    Added support for Swift Package Manager (#193)
    * Added support for Swift Package Manager
    
    * Fixes linter errors and Copyright message
    
    * Fixes .gitignore
    
    * Removed useless files
    
    * Changed default Sources directory
    
    * Added supported platforms

--- Package.swift
+++ Package.swift
@@ -1,3 +1,4 @@
+// swift-tools-version:5.0
 //===----------------------------------------------------------------------===//
 //
 // This source file is part of the DeviceKit open source project
@@ -11,4 +12,30 @@
 
 import PackageDescription
 
-let package = Package(name: "DeviceKit")
+let package = Package(
+    name: "DeviceKit",
+    platforms: [
+        .iOS(.v8),
+        .tvOS(.v9),
+        .watchOS(.v2)
+    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "DeviceKit",
+            targets: ["DeviceKit"])
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "DeviceKit",
+            dependencies: [],
+            path: "Source"),
+        .testTarget(
+            name: "DeviceKitTests",
+            dependencies: ["DeviceKit"],
+            path: "Tests")
+    ],
+    swiftLanguageVersions: [.v5]
+)
