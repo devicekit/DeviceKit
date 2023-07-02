e2dca7718776920f42840ede4b98e9ab5e9a9f33
Zandor Smith <info@zsinfo.nl>
2023-07-02 11:51:57 +0200

    Adds compilation support for visionOS.
    Fixes https://github.com/devicekit/DeviceKit/issues/355

--- Source/Device.generated.swift
+++ Source/Device.generated.swift
@@ -931,12 +931,16 @@ public enum Device {
 
     public var isZoomed: Bool? {
       guard isCurrent else { return nil }
+      #if os(xrOS)
+      return nil
+      #else
       if Int(UIScreen.main.scale.rounded()) == 3 {
         // Plus-sized
         return UIScreen.main.nativeScale > 2.7 && UIScreen.main.nativeScale < 3
       } else {
         return UIScreen.main.nativeScale > UIScreen.main.scale
       }
+      #endif
     }
 
     /// All Touch ID Capable Devices
@@ -1309,7 +1313,7 @@ public enum Device {
 
   /// The brightness level of the screen.
   public var screenBrightness: Int {
-    #if os(iOS)
+    #if os(iOS) && !os(xrOS)
     return Int(UIScreen.main.brightness * 100)
     #else
     return 100
@@ -1706,7 +1710,7 @@ extension Device.BatteryState: Comparable {
 }
 #endif
 
-#if os(iOS)
+#if os(iOS) && !os(xrOS)
 extension Device {
   // MARK: Orientation
     /**
