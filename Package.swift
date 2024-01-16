116613f142d14d92a3a366efa7fde8c6c1b5f1b4
Ruslan Timchenko <rustimchek@mail.ru>
2024-01-16 12:33:40 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 //===----------------------------------------------------------------------===//
 //
 // This source file is part of the DeviceKit open source project
